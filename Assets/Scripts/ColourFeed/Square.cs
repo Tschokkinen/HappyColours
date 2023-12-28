c7ebc02790d5376eb4371decd9d633f0b4f7e149
Gavril Tschokkinen <80984067+Tschokkinen@users.noreply.github.com>
2023-12-28 21:45:49 +0200

    Add files via upload

--- Assets/Scripts/ColourFeed/Square.cs
+++ Assets/Scripts/ColourFeed/Square.cs
@@ -22,7 +22,6 @@ namespace ColourFeed
             extents = spriteRenderer.bounds.extents;
 
             ColourText();
-            // TouchControls.screenDrag += Move;
         }
 
         private void ColourText()
@@ -34,41 +33,13 @@ namespace ColourFeed
         // Update is called once per frame
         void Update()
         {
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                gameObject.transform.position = new Vector3(transform.position.x, (transform.position.y + extents.y));
-            }
-
             if (this.gameObject.transform.position.y >= 15.0f)
             {
                 SquareDestroyed?.Invoke(this, EventArgs.Empty);
                 Debug.Log(gameObject.transform.position);
                 Destroy(this.gameObject);
             }
-
-
-
         }
-
-        // IEnumerator LerpColor()
-        // {
-        //     float i = 0.0f;
-        //     while (i < 20000)
-        //     {
-        //         Color rnd1 = spriteRenderer.color;
-        //         Color rnd2 = UnityEngine.Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
-        //         Color lerped = Color.Lerp(rnd1, rnd2, Mathf.PingPong(Time.time, 1));
-        //         spriteRenderer.color = lerped;
-        //         i++;
-        //     }
-
-        //     yield return null;
-        // }
-
-        // private void Move(object sender, EventArgs e)
-        // {
-        //     this.gameObject.transform.position = new Vector3(transform.position.x, transform.position.x + 1.0f, transform.position.z);
-        // }
     }
 }
 
